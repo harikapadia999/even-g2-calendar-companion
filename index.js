8bb0759c1b2afbb4b9649840709950d3f2fce721
HARI KAPADIA <99083489+harikapadia999@users.noreply.github.com>
2026-02-16 11:28:19 +0530

    feat: add React Native entry point

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Even G2 Calendar Companion
+ * React Native Entry Point
+ * 
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+
+// Register the main application component
+AppRegistry.registerComponent(appName, () => App);
